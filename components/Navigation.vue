7dc5e25199b8b08a731eeed186cb9abf6deb2ca3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-22 14:16:06 +0530

    add a header.

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,14 +1,19 @@
 <template>
-    <nav class="bg-white shadow">
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    <div class="flex bg-black">
-        <div>LOGO</div>
-        <div>About</div>
-        <div>Contact</div>
+    <div class="bg-white">
+
+        <header class="relative bg-white">
+            <div class="flex items-center justify-between h-10 bg-black px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
+                <div>CAD</div>
+                <div class="flex gap-4">
+                    <span>Sign in</span>
+                    <div class="h-4 w-px bg-white/30"></div>
+                    <span>Create an Account</span>
+                </div>
+            </div>
+        </header>
+
     </div>
-    </nav>
+
 </template>
 
 <script>
