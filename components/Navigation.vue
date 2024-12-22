ef8beb9760ad5f130f955b0eacbaf6ee602f1295
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-22 13:12:18 +0530

    add nav component.

---
+++ components/Navigation.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="bg-white shadow">
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    <div class="flex bg-black">
+        <div>LOGO</div>
+        <div>About</div>
+        <div>Contact</div>
+    </div>
+    </nav>
+</template>
+
+<script>
+
+export default {
+    name: "Navigation"
+}
+</script>
+
+<style scoped>
+
+</style>
