05c8a49eb4b7e997de78aa89a046fa7adb8a7a77
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-29 13:08:03 +0530

    add css file path.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,12 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   colorMode: {
     classSuffix: ''
-  }
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
