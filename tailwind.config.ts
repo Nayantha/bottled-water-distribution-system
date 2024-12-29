909f6df5c33f2050c0e3f37f6d9d1676935cb4a1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-29 16:20:26 +0530

    add hover bg colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    extend: {
-      textColor: {
-        'default': {
-          light: '#111827', // gray-900
-          dark: '#E5E7EB',  // gray-200
-        }
-      },
-      backgroundColor: {
-        'default': {
-          light: 'white',
-          dark: 'black',
-        }
-      }
-    },
-  },
-  plugins: [],
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+    ],
+    theme: {
+        extend: {
+            textColor: {
+                'default': {
+                    light: '#111827', // gray-900
+                    dark: '#E5E7EB',  // gray-200
+                }
+            },
+            backgroundColor: {
+                'default': {
+                    light: 'white',
+                    dark: 'black',
+                    'hover-light': '#F9FAFB',    // gray-50
+                    'hover-dark': '#111827',     // gray-900
+                }
+            }
+        },
+    }
+    ,
+    plugins: [],
 }
 
