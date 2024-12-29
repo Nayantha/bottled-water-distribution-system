d39d4b7ab140f782c9d9a9ffd628cfed7a2b155b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-29 16:21:30 +0530

    add hover and text colors from theme.

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -13,8 +13,9 @@
             <!--Mobile Menu Icon-->
             <div class="flex lg:hidden gap-4">
                 <ThemeSwitch/>
-                <button class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                        type="button" @click="mobileMenuOpen = true">
+                <button
+                    class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-default-light dark:text-default-dark"
+                    type="button" @click="mobileMenuOpen = true">
                     <span class="sr-only">Open main menu</span>
                     <Bars3Icon aria-hidden="true" class="size-6"/>
                 </button>
@@ -35,7 +36,7 @@
         <Dialog :open="mobileMenuOpen" class="lg:hidden" @close="mobileMenuOpen = false">
             <div class="fixed inset-0 z-50"/>
             <DialogPanel
-                class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-default-light dark:bg-default-dark px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
                     <a class="-m-1.5 p-1.5" href="#">
                         <span class="sr-only">Your Company</span>
@@ -52,12 +53,12 @@
                     <div class="-my-6 divide-y divide-gray-500/10">
                         <div class="space-y-2 py-6">
                             <a v-for="item in navigation" :key="item.name" :href="item.href"
-                               class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{
+                               class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-default-light dark:text-default-dark hover:bg-default-hover-light dark:hover:bg-default-hover-dark">{{
                                     item.name
                                 }}</a>
                         </div>
                         <div class="py-6">
-                            <a class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                            <a class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-default-light dark:text-default-dark dark:hover:bg-default-hover-dark"
                                href="/login">Log in</a>
                         </div>
                     </div>
