d14f017ce67cb001af3bb5f25bb5c38e750d5ffb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-29 13:00:50 +0530

    add button to toggle themes.

---
+++ components/button/ThemeSwitch.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-row items-center justify-center">
+        <button @click="toggleTheme">
+            <Icon :icon="colorMode.preference === 'dark' ? 'radix-icons:moon' : 'radix-icons:sun'"/>
+        </button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import {Icon} from '@iconify/vue';
+
+const colorMode = useColorMode();
+const toggleTheme = () => {
+    colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark';
+};
+</script>
+
+<style scoped>
+
+</style>
