82e3aeb921ba2908f08494da74cac383e5da5062
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-29 13:04:25 +0530

    add nuxt color module.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@
 // @ts-ignore
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  devtools: {enabled: true},
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: ''
+  }
 })
