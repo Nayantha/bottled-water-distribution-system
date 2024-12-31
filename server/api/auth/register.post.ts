9097e46255aef220553b0b5939d2f2ff145bab29
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-31 20:26:56 +0530

    convert the api body to ts class.

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,9 +1,14 @@
 import {PrismaClient} from '@prisma/client'
+import {RegisterUserRequest, RegisterUserRequestInterface} from "~/models/RegisterUserRequest";
 
 const prisma = new PrismaClient()
 const SALT_ROUNDS = 10
 
 export default defineEventHandler(async (event) => {
+
+    const body = await readBody(event);
+
+    const registerUser = RegisterUserRequest.constructFromJson(body as RegisterUserRequestInterface);
 
 
 })
