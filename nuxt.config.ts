4405299e71df9048772a3073911e398b89ca9271
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-31 15:52:43 +0530

    remove vee-validate from nuxt module.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: {enabled: true},
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@vee-validate/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   colorMode: {
     classSuffix: ''
   },
