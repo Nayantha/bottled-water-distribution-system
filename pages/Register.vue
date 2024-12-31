6e040e0fbde65a995deb15623c0860a27e271516
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-31 17:45:55 +0530

    copy ui template from tailwind ui.

--- pages/Register.vue
+++ pages/Register.vue
@@ -1,21 +1,52 @@
 <!-- pages/register.vue -->
 <template>
-    <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-md">
-            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Create your account
-            </h2>
-        </div>
+    <div class="flex min-h-full">
+        <div class="flex flex-1 flex-col justify-center">
+            <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+                <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <div>
+                        <img alt="Your Company" class="mx-auto h-10 w-auto"
+                             src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"/>
+                        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">Sign in to your
+                            account</h2>
+                        <p class="mt-2 text-center text-sm text-gray-600">
+                            Not a member?
+                            <a class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500" href="#">Start a 14
+                                day free trial</a>
+                        </p>
+                    </div>
 
-        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <RegisterForm/>
+                    <div class="mt-10">
+                        <!--form-->
+                        <RegisterForm/>
+
+                        <div class="mt-10">
+                            <div class="relative">
+                                <div class="absolute inset-0 flex items-center">
+                                    <!--line-->
+                                    <div class="w-full border-t border-gray-300"></div>
+                                </div>
+                                <div class="relative flex justify-center text-sm">
+                                    <span class="bg-white px-2 text-gray-500">Or continue with</span>
+                                </div>
+                            </div>
+                            <!--OAuth-->
+                            <OAuth/>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+        <!-- Background image section -->
+        <div class="relative hidden w-0 flex-1 lg:block">
+            <img alt="" class="absolute inset-0 h-full w-full object-cover"
+                 src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"/>
         </div>
     </div>
 </template>
 <script lang="ts" setup>
-import RegisterForm from "~/components/form/RegisterForm.vue";
+import OAuth from "~/components/form/OAuth.vue";
+import RegisterForm from "~/components/form/Register.vue";
 </script>
 
 <style scoped>
