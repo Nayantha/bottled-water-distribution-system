b2a3240d5fc1155c48b54e891bc360f59c363bc8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-31 16:01:09 +0530

    import register form.

--- pages/Register.vue
+++ pages/Register.vue
@@ -1,9 +1,21 @@
+<!-- pages/register.vue -->
 <template>
-  Register
+    <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Create your account
+            </h2>
+        </div>
+
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <RegisterForm/>
+            </div>
+        </div>
+    </div>
 </template>
-
 <script lang="ts" setup>
-
+import RegisterForm from "~/components/form/RegisterForm.vue";
 </script>
 
 <style scoped>
