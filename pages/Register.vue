af4073ddcc43194451dfc7821cdb7751a8df006a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-31 18:01:58 +0530

    add color modes for the dark mode.

--- pages/Register.vue
+++ pages/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex min-h-full">
+    <div class="flex min-h-full dark:bg-default-dark dark:text-default-dark">
         <div class="flex flex-1 flex-col justify-center">
             <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -7,7 +7,7 @@
                         <img alt="Your Company" class="mx-auto h-10 w-auto"
                              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"/>
                         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">Create an Account</h2>
-                        <p class="mt-2 text-center text-sm text-gray-600">
+                        <p class="mt-2 text-center text-sm text-gray-500">
                             Already a member?
                             <a class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                                href="/login">Login</a>
@@ -25,7 +25,8 @@
                                     <div class="w-full border-t border-gray-300"></div>
                                 </div>
                                 <div class="relative flex justify-center text-sm">
-                                    <span class="bg-white px-2 text-gray-500">Or continue with</span>
+                                    <span
+                                        class="bg-white px-2 text-gray-500 dark:bg-default-dark">Or continue with</span>
                                 </div>
                             </div>
                             <!--OAuth-->
