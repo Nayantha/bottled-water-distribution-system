e3eabd9248eb1b890c35ab34ae1f36702228332d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-12-31 18:28:46 +0530

    add dark mode classes.

--- components/form/Register.vue
+++ components/form/Register.vue
@@ -5,31 +5,33 @@
 <template>
     <form class="space-y-6">
         <div>
-            <label class="block text-sm font-medium leading-6 text-gray-900" for="email">Email address</label>
+            <label class="block text-sm font-medium leading-6 text-default-dark dark:text-default-light" for="email">
+                Email address
+            </label>
             <div class="mt-2">
                 <input
-                        id="email"
-                        v-model=""
-                        autocomplete="email"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        name="email"
-                        required
-                        type="email"
+                    id="email"
+                    autocomplete="email"
+                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    name="email"
+                    required
+                    type="email"
                 />
             </div>
         </div>
 
         <div>
-            <label class="block text-sm font-medium leading-6 text-gray-900" for="password">Password</label>
+            <label class="block text-sm font-medium leading-6 text-default-dark dark:text-default-light" for="password">
+                Password
+            </label>
             <div class="mt-2">
                 <input
-                        id="password"
-                        v-model=""
-                        autocomplete="current-password"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        name="password"
-                        required
-                        type="password"
+                    id="password"
+                    autocomplete="current-password"
+                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    name="password"
+                    required
+                    type="password"
                 />
             </div>
         </div>
@@ -38,12 +40,12 @@
             <div class="flex items-center">
                 <input
                         id="remember-me"
-                        v-model=""
                         class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                         name="remember-me"
                         type="checkbox"
                 />
-                <label class="ml-3 block text-sm text-gray-900" for="remember-me">Remember me</label>
+                <label class="ml-3 block text-sm text-default-dark dark:text-default-light" for="remember-me">Remember
+                    me</label>
             </div>
 
             <div class="text-sm">
