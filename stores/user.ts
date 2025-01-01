ae10ecec8fc1f2941e361def88e4cb5b91bc4f9f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-01 15:29:21 +0530

    update the store with the recommended format.

--- stores/user.ts
+++ stores/user.ts
@@ -1,57 +1,24 @@
-import { User } from "~/models/User";
+import type { User } from "~/models/User";
 
-export const useAuthStore = defineStore(
-    'userStore',
-    () => {
-        const token = ref<string | null>(null)
-        const user = ref<User | null>(null)
-        const isAuthenticated = ref<boolean>(false)
+export const useAuthStore = defineStore('user', {
+    state: () => ({
+        user: null as User | null,
+    }),
 
-        const setUser = (newUser: User) => {
-            user.value = newUser;
-            return true
-        }
-
-        const updateUser = (updatedUser: Partial<User>) => {
-            if (!user.value) return
-            Object.assign(user.value, updatedUser)
-        }
-
-        // const getRole = computed(() => user?.value?.role || '');
-
-        const setToken = (accessToken: string) => {
-            token.value = accessToken
-        }
-
-        const setAuthenticated = (user: User) => {
-            isAuthenticated.value = user !== null && user !== undefined
-        }
-
-        const cleanUser = () => {
-            isAuthenticated.value = false
-            user.value = null
-            token.value = ''
-        }
-
-        const getToken = computed(() => token.value)
-
-        return {
-            isAuthenticated,
-            setUser,
-            setToken,
-            user,
-            location,
-            token,
-            setAuthenticated,
-            cleanUser,
-            // getRole,
-            updateUser,
-            getToken,
+    actions: {
+        clearUser() {
+            this.user = null
+        },
+        setUser(newUser: User) {
+            this.user = newUser;
         }
     },
-    {
-        persist: {
-            storage: piniaPluginPersistedstate.localStorage()
-        }
+
+    getters: {
+        isLoggedIn: (state) => !!state.user
     },
-)
+
+    persist: {
+        storage: piniaPluginPersistedstate.localStorage()
+    }
+})
