cee3b80bd6e4db7c842d664de7e276bc99719d78
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-01 13:59:27 +0530

    add Pinia Plugin Persistedstate module.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@pinia/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt',],
   colorMode: {
     classSuffix: ''
   },
