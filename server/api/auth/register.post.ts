dc83d250bd1ce6f8790b3221eece8738818ff7c2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-01 11:15:19 +0530

    parse the api request body to a ts object.

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,7 +1,7 @@
 import { RegisterUserRequest, RegisterUserRequestInterface } from "~/models/RegisterUserRequest";
 
 export default defineEventHandler(async (event) => {
-    // Parse and validate request body
+    // Parse the request body
     const body = await readBody(event);
     const registerUser = RegisterUserRequest.constructFromJson(body as RegisterUserRequestInterface);
 
