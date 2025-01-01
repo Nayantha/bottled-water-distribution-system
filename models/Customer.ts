603f54b9637a83f777a8db848d9a52bacebaa237
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-01 16:52:34 +0530

    add the customer class.

--- models/Customer.ts
+++ models/Customer.ts
@@ -5,3 +5,19 @@ export interface CustomerInterface {
     address: string;
     phone: string;
 }
+
+export class Customer {
+    public readonly id: string;
+    public readonly name: string;
+    public readonly email: string;
+    public readonly address: string;
+    public readonly phone: string;
+
+    public constructor(id: string, name: string, email: string, address: string, phone: string) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+}
