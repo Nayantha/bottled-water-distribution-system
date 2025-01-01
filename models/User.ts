b77b0aaaf00e167413c58696e654d03b4667c6af
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-01 12:09:46 +0530

    add user class.

--- models/User.ts
+++ models/User.ts
@@ -4,3 +4,17 @@ export interface UserInterface {
     email: string;
     id: string;
 }
+
+export class User {
+    public readonly name: string;
+    public readonly username: string;
+    public readonly email: string;
+    public readonly id: string;
+
+    public constructor(name: string, username: string, email: string, id: string) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.id = id;
+    }
+}
