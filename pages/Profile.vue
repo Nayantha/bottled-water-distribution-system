b0e10e1a54de4131c531d13b8669152beb9db40e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-01 15:28:30 +0530

    add profile page.

---
+++ pages/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div v-if="authStore.user.value!!">Loading...</div>
+        <div v-else>
+            Welcome, {{ authStore.user.name }}
+            {{ authStore.user.id }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useAuthStore } from "~/stores/user";
+
+const authStore = useAuthStore();
+</script>
+
+<style scoped>
+
+</style>
