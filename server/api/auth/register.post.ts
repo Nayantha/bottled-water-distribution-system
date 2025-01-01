9fdb9a32277f24568ab97c3c5d1ca262ff9407f8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-01 11:38:58 +0530

    add try catch block and a default error response.

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -5,33 +5,43 @@ import prisma from "~/utils/prisma";
 const SALT_ROUNDS = 10;
 
 export default defineEventHandler(async (event) => {
-    // Parse the request body
-    const body = await readBody(event);
-    const registerUser = RegisterUserRequest.constructFromJson(body as RegisterUserRequestInterface);
+    try {
+        // Parse the request body
+        const body = await readBody(event);
+        const registerUser = RegisterUserRequest.constructFromJson(body as RegisterUserRequestInterface);
 
-    // validate inputs
-    const validityResults = registerUser.validateInputs()
+        // validate inputs
+        const validityResults = registerUser.validateInputs()
 
-    if (!validityResults.valid) {
+        if (!validityResults.valid) {
+            throw createError({
+                statusCode: 400,
+                message: validityResults.message
+            })
+        }
+
+        // Hash password with salt
+        const salt = await bcrypt.genSalt(SALT_ROUNDS)
+        const hashedPassword = await bcrypt.hash(body.password, salt)
+
+        // Create user
+        const user = await prisma.user.create({
+            data: {
+                email: registerUser.email,
+                password: hashedPassword,
+                name: registerUser.name,
+                salt: salt,
+                username: registerUser.username
+            }
+        });
+
+        // return user
+
+    } catch (error: any) {
+
         throw createError({
-            statusCode: 400,
-            message: validityResults.message
+            statusCode: 500,
+            message: 'An error occurred during registration.'
         })
     }
-
-    // Hash password with salt
-    const salt = await bcrypt.genSalt(SALT_ROUNDS)
-    const hashedPassword = await bcrypt.hash(body.password, salt)
-
-    // Create user
-    const user = await prisma.user.create({
-        data: {
-            email: registerUser.email,
-            password: hashedPassword,
-            name: registerUser.name,
-            salt: salt,
-            username: registerUser.username
-        }
-    });
-
 })
