b394c7d32054cd3ffb03edcf13a518c19c0a8e40
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-02 12:05:36 +0530

    implement token class from implement.

--- types/Tokens.ts
+++ types/Tokens.ts
@@ -2,3 +2,13 @@ export interface TokenInterface {
     accessToken: string;
     refreshToken: string;
 }
+
+class Token implements TokenInterface {
+    public readonly accessToken: string;
+    public readonly refreshToken: string;
+
+    public constructor(accessToken: string, refreshToken: string,) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
