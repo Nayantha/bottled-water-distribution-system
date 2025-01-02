32705296058ae1f72bc5f6fe640bbc0c31f2f514
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-02 11:36:57 +0530

    add jwt verify method.

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -13,3 +13,11 @@ export const generateToken = (userId: string) => {
         { expiresIn: JWT_EXPIRES_IN }
     )
 }
+
+export const verifyToken = (token: string) => {
+    try {
+        return jwt.verify(token, JWT_SECRET)
+    } catch (error) {
+        return null
+    }
+}
